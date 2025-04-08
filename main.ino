8e1d40dc07ee93713b53455e13f3227047961d2b
John Jensen <john.jensen.2@gmail.com>
2025-04-08 18:07:23 -0500

    first commit

---
+++ main.ino
@@ -0,0 +1,18 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello, world!");
+}   
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello, world!");
+  delay(1000);
+}
+// This is a simple Arduino sketch that prints "Hello, world!" to the serial monitor every second.
+// It initializes the serial communication in the setup() function and then enters an infinite loop in the loop() function.
+
+
+    
